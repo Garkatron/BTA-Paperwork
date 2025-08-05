de25b1b9957bff9610f678b098f8401b2c6a03dd
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-08-05 15:41:30 +0200

    No comments

--- src/main/java/deus/paperwork/entities/paperplane/PaperPlaneRenderer.java
+++ src/main/java/deus/paperwork/entities/paperplane/PaperPlaneRenderer.java
@@ -16,21 +16,17 @@ public class PaperPlaneRenderer extends EntityRenderer<EntityPaperPlane> {
 	@Override
 	public void render(Tessellator tessellator, EntityPaperPlane entity, double x, double y, double z, float yaw, float partialTick) {
 		GL11.glPushMatrix();
-		// Posicionar el modelo
 		GL11.glTranslatef((float)x, (float)y, (float)z);
-		GL11.glRotatef(180.0F - yaw, 0.0F, 1.0F, 0.0F); // Rotar según yaw
-		GL11.glScalef(-1.0F, -1.0F, 1.0F); // Escala para orientación
-		float scale = 0.0625F; // Escala estándar de Minecraft
-		GL11.glTranslatef(0.0F, -24.0F * scale, 0.0F); // Ajuste vertical
+		GL11.glRotatef(180.0F - yaw, 0.0F, 1.0F, 0.0F);
+		GL11.glScalef(-1.0F, -1.0F, 1.0F);
+		float scale = 0.0625F;
+		GL11.glTranslatef(0.0F, -24.0F * scale, 0.0F);
 
-		// Cargar textura (ajusta la ruta según tu mod)
 		this.bindTexture("/assets/paperwork/textures/entity/paperplane/texture.png");
 
-		// Habilitar configuraciones de renderizado
-		GL11.glEnable(32826); // GL_RESCALE_NORMAL
-		GL11.glEnable(3008);  // GL_ALPHA_TEST
+		GL11.glEnable(32826);
+		GL11.glEnable(3008);
 
-		// Renderizar el modelo
 		this.modelPaperPlane.render(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, scale);
 
 		GL11.glDisable(32826);
